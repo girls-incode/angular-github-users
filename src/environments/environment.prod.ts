ea9c1afc03220c4968a47c219dd0266835f31637
Angelescu Violeta <violeta@girlsincode.com>
2020-11-28 23:59:30 +0100

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   GITHUB_API_URL: 'https://api.github.com',
-  GITHUB_API_KEY: '',
+  GITHUB_API_KEY: '6e30aa99d22df6b8e3746957b0fb893ad9eb2ec8',
 };
